12ec35d5c7d537f6551663e59c5198f127e9d2b5
GabrielaHristov <143615204+GabrielaHristov@users.noreply.github.com>
2023-09-02 20:34:58 +0300

    create web map; add home button

--- map.js
+++ map.js
@@ -4,7 +4,8 @@ require([
     "esri/views/MapView",
     "esri/widgets/ScaleBar",
     "esri/widgets/Legend",
-  ], function(esriConfig, WebMap, MapView, ScaleBar, Legend) {
+    "esri/widgets/Home"
+  ], function(esriConfig, WebMap, MapView, ScaleBar, Legend,Home) {
     esriConfig.apiKey = "AAPKba72e0ab5a174570a5d7166a8626a797Rd6BTFGhgavDcZZV9osrEfz6Q4U0Dxu4y4Z7CTX6pgovODIt6ttFxwcQTXWMXvTB"
 
    const webMap = new WebMap({
@@ -17,5 +18,11 @@ require([
    container: "viewDiv",
    map: webMap 
    });
+
+   const homeBtn = new Home({
+       view: view
+   })
+
+   view.ui.add(homeBtn, "top-left")
 
 })
